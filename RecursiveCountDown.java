f5c23601f35a9302d2c3b8e56a236096b2904bbc
MjBembe <61251576+MjBembe@users.noreply.github.com>
2020-04-08 19:21:46 +0200

    Add files via upload

---
+++ RecursiveCountDown.java
@@ -0,0 +1,15 @@
+public class RecursiveCountDown{
+	public static void main(String[] args){
+		countDown(4);
+	}
+	public static void countDown(int num){
+		if(num<=0){
+			System.out.println();
+		}
+		else {
+			System.out.println(num); 
+			
+		}
+	}
+
+}
